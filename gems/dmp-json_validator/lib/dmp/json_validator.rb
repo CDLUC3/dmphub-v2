b94ae07d4f642272a519c769d7adf1a986031a02
briri <briley@ucop.edu>
2022-05-25 23:05:50 +0000

    added rspec tests

--- gems/dmp-json_validator/lib/dmp/json_validator.rb
+++ gems/dmp-json_validator/lib/dmp/json_validator.rb
@@ -15,17 +15,17 @@ module Dmp
     class << self
       def validate(mode:, json: {})
         json = prepare_json(json: json)
-        return respond unless json.present? && VALIDATION_MODES.include?(mode)
+        return respond if json.nil? || !VALIDATION_MODES.include?(mode)
 
         # Load the appropriate JSON schema for the mode
         schema = load_schema(mode: mode)
-        return respond(message: 'Invalid mode or no JSON schema available!') unless schema.present?
+        return respond(errors: 'No JSON schema available!') if schema.nil?
 
         # Validate the JSON
         errors = JSON::Validator.fully_validate(schema, json)
-        respond(valid: errors.empty?, message: errors)
+        respond(valid: errors.empty?, errors: errors)
       rescue JSON::Schema::ValidationError => e
-        respond(message: "Fatal validation error: #{e.message}")
+        respond(errors: "Fatal validation error: #{e.message}")
       end
 
       private
@@ -34,22 +34,26 @@ module Dmp
       # an array of :errors
       def respond(valid: false, errors: 'JSON was empty or an invalid mode was specified!')
         errors = [errors] unless errors.is_a?(Array)
-        { valid: valid, message: errors }.to_json
+        { valid: %w[true 1].include?(valid.to_s), errors: errors }.to_json
       end
 
       # Load the JSON schema that corresponds with the mode
       def load_schema(mode:)
         filename = "#{Dir.pwd}/config/schemas/#{mode}.json"
-        return nil unless mode.present? && File.exist?(filename)
+        return nil if mode.nil? || !File.exist?(filename)
 
         JSON.parse(File.read(filename))
-      rescue JSON::ParserError
+      rescue JSON::ParserError => e
+        pp e.message
+
         nil
       end
 
-      # Parse the incoming JSON if necessary
+      # Parse the incoming JSON if necessary or return as is if it's already a Hash
       def prepare_json(json:)
-        json.is_a?(String) ? JSON.parse(json) : json
+        return json if json.is_a?(Hash)
+
+        json.is_a?(String) ? JSON.parse(json) : nil
       rescue JSON::ParserError
         nil
       end
--- gems/dmp-json_validator/spec/dmp/json_validator_spec.rb
+++ gems/dmp-json_validator/spec/dmp/json_validator_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe Dmp::JsonValidator do
   it 'has a version number' do
     expect(Dmp::JsonValidator::VERSION).not_to be nil
@@ -8,4 +9,162 @@ RSpec.describe Dmp::JsonValidator do
   it 'returns the validation modes' do
     expect(Dmp::JsonValidator::VALIDATION_MODES.is_a?(Array)).to eq(true)
   end
+
+  it 'does not allow the validation modes to be altered' do
+    expect { Dmp::JsonValidator::VALIDATION_MODES << 'foo' }.to raise_error(FrozenError)
+  end
+
+  describe 'validate(mode:, json:)' do
+    before(:each) do
+      @expected_error = JSON.parse({
+        valid: false, errors: ['JSON was empty or an invalid mode was specified!']
+      }.to_json)
+      @json = { 'foo': 'bar' }
+      @schema = {
+        "type": 'object',
+        "properties": {
+          "foo": {
+            "$id": '#/properties/foo',
+            "type": 'string'
+          }
+        },
+        "required": [
+          'foo'
+        ]
+      }
+    end
+
+    it 'returns the appropriate error when no :mode is specified' do
+      allow(Dmp::JsonValidator).to receive(:prepare_json).and_return(@json)
+      result = JSON.parse(Dmp::JsonValidator.validate(mode: nil, json: @json))
+      expect(compare_hashes(hash_a: result, hash_b: @expected_error)).to eql(true)
+    end
+    it 'returns the appropriate error when an invalid :mode is specified' do
+      allow(Dmp::JsonValidator).to receive(:prepare_json).and_return(@json)
+      result = JSON.parse(Dmp::JsonValidator.validate(mode: 'foo', json: @json))
+      expect(compare_hashes(hash_a: result, hash_b: @expected_error)).to eql(true)
+    end
+    it 'returns the appropriate error when prepare_json returns a nil' do
+      allow(Dmp::JsonValidator).to receive(:prepare_json).and_return(nil)
+      result = JSON.parse(Dmp::JsonValidator.validate(mode: 'author', json: @json))
+      expect(compare_hashes(hash_a: result, hash_b: @expected_error)).to eql(true)
+    end
+    it 'returns the appropriate error when load_schema returns a nil' do
+      allow(Dmp::JsonValidator).to receive(:prepare_json).and_return(@json)
+      allow(Dmp::JsonValidator).to receive(:load_schema).and_return(nil)
+      @expected_error['errors'] = ['No JSON schema available!']
+      result = JSON.parse(Dmp::JsonValidator.validate(mode: 'author', json: @json))
+      expect(compare_hashes(hash_a: result, hash_b: @expected_error)).to eql(true)
+    end
+    it 'returns the appropriate error if the :json is NOT valid' do
+      @json = { 'bar': 'foo' }
+      allow(Dmp::JsonValidator).to receive(:prepare_json).and_return(@json)
+      allow(Dmp::JsonValidator).to receive(:load_schema).and_return(@schema)
+      result = JSON.parse(Dmp::JsonValidator.validate(mode: 'author', json: @json))
+      expect(result['valid']).to eql(false)
+      expect(result['errors'].first.include?('required property of \'foo\' in schema')).to eql(true)
+    end
+    it 'returns the appropriate error if the :json is valid' do
+      allow(Dmp::JsonValidator).to receive(:prepare_json).and_return(@json)
+      allow(Dmp::JsonValidator).to receive(:load_schema).and_return(@schema)
+      result = JSON.parse(Dmp::JsonValidator.validate(mode: 'author', json: @json))
+      expected = JSON.parse({ valid: true, errors: [] }.to_json)
+      expect(compare_hashes(hash_a: result, hash_b: expected)).to eql(true)
+    end
+  end
+
+  describe 'private methods' do
+    describe 'respond(valid:, errors:)' do
+      it 'returns the expected JSON if :valid and :errors are not provided' do
+        expected = {
+          valid: false, errors: ['JSON was empty or an invalid mode was specified!']
+        }.to_json
+        expect(Dmp::JsonValidator.send(:respond)).to eql(expected)
+      end
+      it 'converts :errors to an Array if a string is provided' do
+        expected = { valid: false, errors: ['foo'] }.to_json
+        expect(Dmp::JsonValidator.send(:respond, errors: 'foo')).to eql(expected)
+      end
+      it 'returns { valid: false } if :valid is not true' do
+        expected = {
+          valid: false, errors: ['JSON was empty or an invalid mode was specified!']
+        }.to_json
+        expect(Dmp::JsonValidator.send(:respond, valid: 'false')).to eql(expected)
+        expect(Dmp::JsonValidator.send(:respond, valid: 'foo')).to eql(expected)
+        expect(Dmp::JsonValidator.send(:respond, valid: [true])).to eql(expected)
+        expect(Dmp::JsonValidator.send(:respond, valid: false)).to eql(expected)
+        expect(Dmp::JsonValidator.send(:respond, valid: 0)).to eql(expected)
+      end
+      it 'returns the expected JSON' do
+        expected = { valid: false, errors: ['foo'] }.to_json
+        expect(Dmp::JsonValidator.send(:respond, valid: false, errors: ['foo'])).to eql(expected)
+        expected = { valid: true, errors: ['foo'] }.to_json
+        expect(Dmp::JsonValidator.send(:respond, valid: true, errors: ['foo'])).to eql(expected)
+        expected = { valid: true, errors: %w[foo bar] }.to_json
+        expect(Dmp::JsonValidator.send(:respond, valid: true, errors: %w[foo bar])).to eql(expected)
+      end
+    end
+
+    describe 'load_schema(mode:)' do
+      it 'returns nil if :mode is not provided' do
+        expect(Dmp::JsonValidator.send(:load_schema, mode: nil)).to eql(nil)
+      end
+      it 'returns nil if :mode is not a valid mode' do
+        expect(Dmp::JsonValidator.send(:load_schema, mode: 'foo')).to eql(nil)
+      end
+      it 'returns nil if :mode has no corresponding JSON schema' do
+        allow(File).to receive(:exist?).and_return(false)
+        expect(Dmp::JsonValidator.send(:load_schema, mode: :author)).to eql(nil)
+      end
+      it 'returns nil if contents of JSON schema are not parseable' do
+        allow(File).to receive(:read).and_return('/{foo:"4%Y"$%\/')
+        expect(Dmp::JsonValidator.send(:load_schema, mode: :author)).to eql(nil)
+      end
+      it 'returns the JSON schema' do
+        schema = Dmp::JsonValidator.send(:load_schema, mode: :author)
+        expected = 'https://github.com/CDLUC3/dmphub-v2/gems/dmp-json_validator/config/schemas/author.json'
+        expect(schema['$id']).to eql(expected)
+
+        schema = Dmp::JsonValidator.send(:load_schema, mode: :amend)
+        expected = 'https://github.com/CDLUC3/dmphub-v2/gems/dmp-json_validator/config/schemas/amend.json'
+        expect(schema['$id']).to eql(expected)
+
+        schema = Dmp::JsonValidator.send(:load_schema, mode: :delete)
+        expected = 'https://github.com/CDLUC3/dmphub-v2/gems/dmp-json_validator/config/schemas/delete.json'
+        expect(schema['$id']).to eql(expected)
+      end
+    end
+
+    describe 'prepare_json(json:)' do
+      it 'returns nil if :json is not provided' do
+        expect(Dmp::JsonValidator.send(:prepare_json, json: nil)).to eql(nil)
+      end
+      it 'parses the JSON if it is a String' do
+        expected = JSON.parse({ foo: 'bar' }.to_json)
+        expect(Dmp::JsonValidator.send(:prepare_json, json: '{"foo":"bar"}')).to eql(expected)
+      end
+      it 'returns nil if :json is not parseable JSON' do
+        expect(Dmp::JsonValidator.send(:prepare_json, json: '/{foo:"4%Y"$%\/')).to eql(nil)
+      end
+      it 'returns nil if :json is not a Hash or a String' do
+        expect(Dmp::JsonValidator.send(:prepare_json, json: 1.34)).to eql(nil)
+      end
+      it 'returns the :json as is if it ia a Hash' do
+        expected = { foo: 'bar' }
+        expect(Dmp::JsonValidator.send(:prepare_json, json: expected)).to eql(expected)
+      end
+    end
+  end
+
+  # Helper function that compares 2 hashes regardless of the order of their keys
+  def compare_hashes(hash_a: {}, hash_b: {})
+    a_keys = hash_a.keys.sort { |a, b| a <=> b }
+    b_keys = hash_b.keys.sort { |a, b| a <=> b }
+    return false unless a_keys == b_keys
+
+    valid = true
+    a_keys.each { |key| valid = false unless hash_a[key] == hash_b[key] }
+    valid
+  end
 end
+# rubocop:enable Metrics/BlockLength

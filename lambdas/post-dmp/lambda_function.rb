1eaf3b31b449a31196abe3b674afa1c429944854
briri <brian.riley@ucop.edu>
2022-07-15 08:09:39 -0700

    Added logic for create DMP

--- lambdas/post-dmp/lambda_function.rb
+++ lambdas/post-dmp/lambda_function.rb
@@ -1,39 +1,50 @@
 # require 'httparty'
 require 'json'
+require 'dmp/authorizer'
+require 'dmp/dynamo_adapter'
 require 'dmp/json_validator'
+require 'dmp/response_formatter'
+
+# Parameters
+# ----------
+# event: Hash, required
+#     API Gateway Lambda Proxy Input Format
+#     Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+# context: object, required
+#     Lambda Context runtime methods and attributes
+#     Context doc: https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
+
+# Returns
+# ------
+# API Gateway Lambda Proxy Output Format: dict
+#     'statusCode' and 'body' are required
+#     # api-gateway-simple-proxy-for-lambda-output-format
+#     Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+
+MSG_DEFAULT = 'Unable to process your request.'.freeze
+MSG_DISABLED = 'This operation is temporarily unavailable.'.freeze
+MSG_EXISTS = 'DMP already exists.'.freeze
 
 def lambda_handler(event:, context:)
-  # Sample pure Lambda function
+  responder = Dmp::ResponseFormatter
+  dynamo = Dmp::DynamoAdapter.new(provenance: event.caller, debug: event.debug)
 
-  # Parameters
-  # ----------
-  # event: Hash, required
-  #     API Gateway Lambda Proxy Input Format
-  #     Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+  # Make sure the JSON is valid
+  mode = Dmp::JsonValidator::VALIDATION_MODES.select { |item| item == 'author' }
+  resp = Dmp::JsonValidator.validate(mode: mode, json: event.payload)
+  return responder.format_response(status: 400, errors: resp[:errors]) unless resp[:valid]
 
-  # context: object, required
-  #     Lambda Context runtime methods and attributes
-  #     Context doc: https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
+  # Make sure the caller is authorized to do this
+  action = Dmp::Authorizer::ACTION_TYPES.select { |item| item == 'create' }
+  resp = Dmp::Authorizer.authorize(
+    provenance: event.caller, env: event.env, action: action, dmp: event.payload
+  )
+  return responder.format_response(status: 400, errors: resp[:errors]) unless resp[:authorized]
 
-  # Returns
-  # ------
-  # API Gateway Lambda Proxy Output Format: dict
-  #     'statusCode' and 'body' are required
-  #     # api-gateway-simple-proxy-for-lambda-output-format
-  #     Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+  # Create the DMP
+  resp = dynamo.create(json: event.payload)
+  return responder.format_response(status: resp[:status], errors: resp[:error]) if resp[:status] != 201
 
-  # begin
-  #   response = HTTParty.get('http://checkip.amazonaws.com/')
-  # rescue HTTParty::Error => error
-  #   puts error.inspect
-  #   raise error
-  # end
-
-  {
-    statusCode: 200,
-    body: {
-      message: "Hello World! You are trying to Create a DMP. #{Dmp::JsonValidator::VALIDATION_MODES}",
-      # location: response.body
-    }.to_json
-  }
+  responder.format_response(status: 201, dmps: resp[:items])
 end
